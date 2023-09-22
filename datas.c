88c45388034bdbb6f3ec0dfc43dd9f1bb4725197
Luiz Gonzaga Franco Michelmann <39537987+lgfranco22@users.noreply.github.com>
2023-09-22 18:05:15 -0300

    Create datas.c

---
+++ datas.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *resul;
+
+int main(int argc, char *argv[])
+{
+
+  if(argc != 3)
+  {
+    printf("Modo de uso: datas <year-min> <year-max>\n");
+    exit(0);
+  }
+  else{
+  int aa, mm, dd;
+  
+  int ini = atoi(argv[1]);
+  int fin = atoi(argv[2]);
+  
+  printf("Gerando...\n");
+  resul = fopen("datas.txt", "w");
+
+  for (aa = ini; aa < fin; aa++)
+    for (mm = 1; mm < 13; mm ++)
+      for ( dd = 1; dd < 32; dd++)
+      {
+        fprintf(resul, "%02d%02d%02d\n", aa, mm, dd);// 19900101
+        fprintf(resul, "%02d%02d%02d\n", dd, mm, aa);// 01011990
+
+        fprintf(resul, "%02d-%02d-%02d\n", aa, mm, dd);// 1990-01-01
+        fprintf(resul, "%02d-%02d-%02d\n", dd, mm, aa);// 01-01-1990
+
+        fprintf(resul, "%02d/%02d/%02d\n", aa, mm, dd);// 1990/01/01
+        fprintf(resul, "%02d/%02d/%02d\n", dd, mm, aa);// 01/01/1990
+
+        fprintf(resul, "%02d\\%02d\\%02d\n", aa, mm, dd);// 1990\01\01
+        fprintf(resul, "%02d\\%02d\\%02d\n", dd, mm, aa); // 01\01\1990
+
+      }
+
+  fclose(resul);
+  system("clear");
+  printf("Finalizado.\n");
+  return 0;
+  }
+}
